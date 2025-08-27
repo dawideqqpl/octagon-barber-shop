fb00cc23159c26e93401875c887ff24290d04f01
dawideqq <dawidzielinski.main@gmail.com>
2025-08-27 22:27:37 +0200

    Official deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//test comment
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('home');
